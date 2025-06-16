5f74ef3e785abf151819e9fccc6fd916c0629fd1
Alexa Koeck <alexakoeck@hanyang.ac.kr>
2025-06-16 16:42:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ def read_data(data: str | None = None):
 
 # Optional for direct run
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
