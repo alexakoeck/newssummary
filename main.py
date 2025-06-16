027fad75b295073279d4b2cb72f5c718bcd48ce4
Alexa Koeck <alexakoeck@hanyang.ac.kr>
2025-06-16 09:08:26 +0200

    main.py

--- main.py
+++ main.py
@@ -170,7 +170,7 @@ print('test1')
 # Load the English model
 nlp = spacy.load("en_core_web_sm")
 # Process the text
-doc = nlp(query)
+doc = nlp(response)
 
 # Extract named entities
 entities = [(ent.text, ent.label_) for ent in doc.ents]
