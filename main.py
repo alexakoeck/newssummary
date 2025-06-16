e1b4bdcc9b6e47acf0aaf5be4072304dab7a7986
Alexa Koeck <alexakoeck@hanyang.ac.kr>
2025-06-16 17:01:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def read_data(data: str | None = None):
     response= response(data)
     return {"summary": response}
 
+
 # Optional for direct run
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080)
